b724104b379a8635b7d51fc8a620563660ca893e
joshsimard <joshuasimard@gmail.com>
2019-09-27 01:56:39 -0500

    added my first item handling

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const Discord = require('discord.js');
 const client  = new Discord.Client();
 const auth    = require('./auth.json');
+const items   = require('./items/level_1.json');
+
+const _getTraitString = function ( traits ) {
+	let result = "";
+
+	traits.forEach( trait => { result += trait + ", "; })
+
+	result = result.substring( 0, result.length -2 );
+
+	return result;
+};
+
+const _formatItem = function ( item ) {
+
+	return `
+	\`\`\`bash
+	"${item.name.toUpperCase( )}" (Item ${item.level})
+	--------------------------------------
+	${_getTraitString( item.traits )}
+
+	Price: "${item.price}"
+	Description: "${item.description}"
+	--------------------------------------
+	\`\`\`
+	`; 
+};
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
@@ -13,8 +39,15 @@ client.on('message', msg => {
 
 		args = args.splice(1);
 		switch( cmd ) {
+			case "random":
+				let index = Math.floor ( Math.random() * Object.keys( items ).length );
+				msg.reply( _formatItem( items[index]) );
+				break;
 			case "help":
-				msg.reply('This bot is under construction.');
+				msg.reply(`
+				Current commands include:
+					!random: displays a random item.
+				`);
 				break;
 			default:
 				msg.reply('Command not recognized. Try !help for available commands.');
